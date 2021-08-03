6c72d04963c19b4f2d1f09a701f2871f60550233
toleksa <toleksa@gmail.com>
2021-08-03 05:03:07 +0000

    move core things to kube-system

--- install-metallb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/metallb.yaml
-
